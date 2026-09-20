861b25cf0862cd35aed00b062e65ec2fd05acdb7
amiralinejadseyedi <alinezhad.amir11@gmail.com>
2026-09-20 20:01:33 +0330

    third day pydantic

--- core/main.py
+++ core/main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI, Query, status, HTTPException
 from fastapi.responses import JSONResponse
 import random
 from  dataclasses import dataclass
+from schemas import PersonCreateSchema, PersonResponeSchema, PersonUpdateSchema 
+from typing import List
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return JSONResponse(content = {"message":"Hello World"}, status_code = status.HTTP_202_ACCEPTED)
+    return JSONResponse(content = {"message":"Hello World"}, status_code  = status.HTTP_202_ACCEPTED)
 
 name_list =[ 
 {"id":1 ,"name":"ali" },
@@ -18,13 +21,14 @@ name_list =[
 
 
 
-@app.get("/name")
-def search_name(q:str|None = Query(default = None , max_length=50)):
-    if q:
+@app.get("/name", response_model=List[PersonResponeSchema])
+def retrieve_name_list(q : str| None = Query(deprecated=True)):
+    if q :
         return [item for item in name_list if q == item["name"]]
     return name_list
 
-@app.get("/name/{name_id}", status_code = status.HTTP_200_OK)
+
+@app.get("/name/{name_id}", status_code = status.HTTP_200_OK,response_model = PersonResponeSchema )
 def retrieve_name_detail(name_id:int):
     for name in name_list:
         if name["id"] == name_id:
@@ -40,25 +44,26 @@ class Student:
 @dataclass
 class ResponseStudent:
     id:int
-    name:str           
+    name:str 
 
-@app.post("/name",status_code = status.HTTP_201_CREATED)
+
+              
+
+@app.post("/name",status_code = status.HTTP_201_CREATED, response_model=PersonResponeSchema)
 #def create_name(name:str):
-def create_name(student:Student):
-    name_obj = {"id":random.randint(6,100), "name": student.name}
+def create_name(person : PersonCreateSchema):
+    name_obj = {"id":random.randint(6,100), "name": person.name}
     name_list.append(name_obj)
-    response = ResponseStudent(random.randint(10,100), student.name)
-    print(response)
-
+   
     return name_obj
 
 
 
-@app.put("/name/{name_id}")
-def update_name_detail(name_id:int, name:str):
+@app.put("/name/{name_id}", response_model=PersonResponeSchema)
+def udate_name_detail(name_id:int, person:PersonUpdateSchema):
     for item in name_list:
         if item["id"]==name_id:
-            item["name"] = name
+            item["name"] = person.name
             return item
     return {"detail":"object not found"}
 
